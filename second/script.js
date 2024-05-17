b836a3c58fb9175c78f6ae84deab06f0f0ad88c9
Leeseayoung <tnfusrltk12@naver.com>
2024-05-17 14:13:29 +0900

    완료!

--- second/script.js
+++ second/script.js
@@ -1,3 +1,12 @@
+// 이거 잘 모르겠다! 다시 복습
+let container = document.getElementById("container");
+
+container.addEventListener("click", function (event) {
+  if (event.target.classList.contains("box")) {
+    event.target.classList.toggle("clicked");
+  }
+});
+
 function getSquare(number) {
   return number * number;
 }
